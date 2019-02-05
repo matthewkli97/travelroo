29c0ac8e2f48991655cdfaf9a51d15878b420cac
Matthew Li <matthewli@Matthews-MacBook-Pro.local>
2019-02-04 21:39:01 -0800

    Updates

---
+++ precache-manifest.adaf6dd7be9668f8ead3e31771ac8710.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e8dc8f31d289f8772ae",
+    "url": "/travelbear/static/css/main.e5aaf5f4.chunk.css"
+  },
+  {
+    "revision": "1e8dc8f31d289f8772ae",
+    "url": "/travelbear/static/js/main.1e8dc8f3.chunk.js"
+  },
+  {
+    "revision": "deaedff89aabfe5e834f",
+    "url": "/travelbear/static/css/1.33fdd34b.chunk.css"
+  },
+  {
+    "revision": "deaedff89aabfe5e834f",
+    "url": "/travelbear/static/js/1.deaedff8.chunk.js"
+  },
+  {
+    "revision": "707024c88d1bb04ed310",
+    "url": "/travelbear/static/js/runtime~main.707024c8.js"
+  },
+  {
+    "revision": "c18ec00839c56c40c548acbb02aebd60",
+    "url": "/travelbear/static/media/kangaroo.c18ec008.svg"
+  },
+  {
+    "revision": "d1b60a0f47fd79cd1425ad0a12de8e82",
+    "url": "/travelbear/static/media/egypt-pyramid.d1b60a0f.svg"
+  },
+  {
+    "revision": "d59385b98a8fc02fdbd85de8a5734b3b",
+    "url": "/travelbear/static/media/loupe.d59385b9.svg"
+  },
+  {
+    "revision": "b51baa1104206010491612f44a65b6a0",
+    "url": "/travelbear/static/media/travel.b51baa11.svg"
+  },
+  {
+    "revision": "308015275293543c165df92c7176c8ae",
+    "url": "/travelbear/static/media/route.30801527.svg"
+  },
+  {
+    "revision": "20083396ec9ee64abc41d5701d06ce4b",
+    "url": "/travelbear/static/media/Matthew.20083396.jpeg"
+  },
+  {
+    "revision": "45b4f7980b2eec3fbce936384fc44fa2",
+    "url": "/travelbear/static/media/Peter.45b4f798.jpg"
+  },
+  {
+    "revision": "1d28fae8dd6521c2e8a65fea6da42c3a",
+    "url": "/travelbear/static/media/Eric.1d28fae8.jpg"
+  },
+  {
+    "revision": "41aac9fdb20719682164f43f62872955",
+    "url": "/travelbear/static/media/Alisa.41aac9fd.jpg"
+  },
+  {
+    "revision": "1ec147cf6f77dff137d9be6982fba87f",
+    "url": "/travelbear/static/media/ischool.1ec147cf.svg"
+  },
+  {
+    "revision": "4d74bb12e70dffa8b8da7a58e1a0c475",
+    "url": "/travelbear/static/media/uw.4d74bb12.png"
+  },
+  {
+    "revision": "9ec499779351b8fb412061303b9400a4",
+    "url": "/travelbear/index.html"
+  }
+];
